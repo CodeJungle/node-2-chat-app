801b8a74b65518084b5b5d75a6b88679cf9e6509
Michael Sansom <msansomcomputers@hotmail.co.uk>
2017-04-29 09:59:43 +0100

    Added geolocation support via geolocation api for share location button

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,27 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     var from = 'Michael';
     var text = 'This is a test';
-    var message = generateMessage(from, text)
+    var message = generateMessage(from, text);
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Mark';
+    var latitude = 15;
+    var longitude = 19;
+    var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
